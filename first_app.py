9c044d885bc0714dd842bdb32785853987b5e65d
root <root@M30528.localdomain>
2023-02-03 14:30:07 +0900

    First app

--- first_app.py
+++ first_app.py
@@ -1,7 +1,8 @@
-#<pythonfile>.ipynb notebook
-#프로그램 작성시에는 pythonfile.py
-#python3 pythonfile.py
-#streamlit run <streamlitapp>.py
+# <pythonfile>.ipynb notebook
+# 프로그램 작성시에는 <pythonfile>.py
+# python3 <pythonfile>.py
+# streamlit run <streamlitapp>.py 
+
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -33,6 +34,14 @@ def temp_map():
 
 def main():
     st.title("First_APP")
+
+    st.sidebar.write('''
+    # lab1
+    # lab2
+    - lab3
+    - lab4
+    ''')
+
     df = pd.DataFrame({
         "first_col":[1, 2, 3, 4],
         "second_col":[10, 20, 30, 40]
